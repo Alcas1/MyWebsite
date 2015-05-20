b2192a2c495f2422b74e30a5bc877ff49f3389ea
Alcas1 <jonathanwu70@gmail.com>
2015-05-19 23:11:04 -0400

    more css changes

--- public/js/ytmenu.js
+++ public/js/ytmenu.js
@@ -1,13 +1,4 @@
-/**
- * ytmenu.js v1.0.0
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2013, Codrops
- * http://www.codrops.com
- */
+
 var YTMenu = (function() {
 
 	function init() {
