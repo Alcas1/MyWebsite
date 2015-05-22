3f5358b3e89b82a277216884ca03ccf1ae1bcebb
Alcas1 <jonathanwu70@gmail.com>
2015-05-22 10:29:32 -0400

    changes to css

--- public/js/ytmenu.js
+++ public/js/ytmenu.js
@@ -22,7 +22,7 @@ var YTMenu = (function() {
 					
 					el.className += ' dr-menu-open';
 					open = true;
-					var fHeight = (document.height !== undefined) ? document.height : document.body.offsetHeight;
+					var fHeight=document.documentElement.scrollHeight;
     				document.getElementById('left_line').style.height=fHeight+"px";
 					shade(open);
 				}
