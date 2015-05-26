958fbd9786a7fb5201beb8108c941e6615933d3d
Alcas1 <jonathanwu70@gmail.com>
2015-05-26 16:10:01 -0400

    changes to css

--- public/js/ytmenu.js
+++ public/js/ytmenu.js
@@ -11,11 +11,8 @@ window.onload = (function() {
 
 					el.className += ' dr-menu-open';
 					open = true;
-					var actualHeight = window.innerHeight ||
-                      document.documentElement.clientHeight ||
-                      document.body.clientHeight ||
-                      document.body.offsetHeight;
-					var fHeight = actualHeight;
+					var D = document;
+					var fHeight =Math.max(D.body.scrollHeight, D.documentElement.scrollHeight, D.body.offsetHeight, D.documentElement.offsetHeight, D.body.clientHeight, D.documentElement.clientHeight);
 					document.getElementById('left_line').style.height = (fHeight - 54) + "px";
 					shade(open, el, event);
 				}
@@ -63,4 +60,4 @@ window.onload = (function() {
 
 	init();
 
-})(); 
+})();
