aba1ee9d8b8d1b4364c6abe5ebd0271d12c7b12e
Alcas1 <jonathanwu70@gmail.com>
2015-05-26 14:10:44 -0400

    changes to css

---
+++ public/js/contact_js.js
@@ -0,0 +1,19 @@
+function init() {
+
+
+	window.onload = function() {
+
+    document.getElementById("contact").onsubmit = function() {
+        var form = this;
+        document.getElementById('contained').className ='animate';
+        document.getElementById('contained2').className ='animate2';
+        document.getElementById('success').className ='success_animate';
+        setTimeout(function() {
+            form.submit();
+        }, 1000); // 3 seconds
+        return false;
+    };
+};
+}
+
+init();
