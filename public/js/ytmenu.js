a2c5e17e72180265dcca57b2d909f6603b8cb6c1
Alcas1 <jonathanwu70@gmail.com>
2015-05-26 15:53:16 -0400

    changes to css

--- public/js/ytmenu.js
+++ public/js/ytmenu.js
@@ -1,80 +1,64 @@
-
-
-
-
 var YTMenu = (function() {
 
 	function init() {
-		
-		
-		
-		
-		
-		[].slice.call( document.querySelectorAll( '.dr-menu' ) ).forEach( function( el, i ) {
 
-			var trigger = el.querySelector( 'div.dr-trigger' ),
-				icon = trigger.querySelector( 'span.dr-icon-menu' ),
-				open = false;
-	
+		[].slice.call(document.querySelectorAll('.dr-menu')).forEach(function(el, i) {
 
-			trigger.addEventListener( 'click', function( event ) {
-				if( !open ) {
-					
+			var trigger = el.querySelector('div.dr-trigger'), icon = trigger.querySelector('span.dr-icon-menu'), open = false;
+
+			trigger.addEventListener('click', function(event) {
+				if (!open) {
+
 					el.className += ' dr-menu-open';
 					open = true;
-					var fHeight=document.documentElement.scrollHeight;
-    				document.getElementById('left_line').style.height=(fHeight-54)+"px";
-					shade(open,el,event);
-				}
-			}, false );
+					var body = document.body, html = document.documentElement;
 
-			icon.addEventListener( 'click', function( event ) {
-				if( open ) {
+					var fHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+					document.getElementById('left_line').style.height = (fHeight - 54) + "px";
+					shade(open, el, event);
+				}
+			}, false);
+
+			icon.addEventListener('click', function(event) {
+				if (open) {
 					event.stopPropagation();
 					open = false;
-					el.className = el.className.replace(/\bdr-menu-open\b/,'');
+					el.className = el.className.replace(/\bdr-menu-open\b/, '');
 					shade(open);
 					return false;
 				}
-			}, false );
-			var cover=document.getElementById('cover');
-			cover.addEventListener('click',function(event)
-			{
-				if(open)
-				{
+			}, false);
+			var cover = document.getElementById('cover');
+			cover.addEventListener('click', function(event) {
+				if (open) {
 					event.stopPropagation();
 					open = false;
-					el.className = el.className.replace(/\bdr-menu-open\b/,'');
+					el.className = el.className.replace(/\bdr-menu-open\b/, '');
 					//(cover.style.overflow-y)="auto";
-					cover.style.opacity="0";
-					cover.style.zIndex="-1";
+					cover.style.opacity = "0";
+					cover.style.zIndex = "-1";
 					return false;
 				}
-				
-			},false);
-		} );
+
+			}, false);
+		});
 
 	}
-	
-	function shade(check,el,event)
-	{
-		var cover=document.getElementById('cover');
-		
-		if(check)
-		{
-			
-			cover.style.opacity=".5";
-			cover.style.zIndex="2";
-		}
-		else
-		{
-			
-			cover.style.opacity="0";
-			cover.style.zIndex="-1";
+
+	function shade(check, el, event) {
+		var cover = document.getElementById('cover');
+
+		if (check) {
+
+			cover.style.opacity = ".5";
+			cover.style.zIndex = "2";
+		} else {
+
+			cover.style.opacity = "0";
+			cover.style.zIndex = "-1";
 		}
 	}
-	
-	
+
 	init();
 
-})();
+})(); 
