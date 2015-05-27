20f97c2b787fc33020e2f2a563ae931494b2065f
Alcas1 <jonathanwu70@gmail.com>
2015-05-27 13:32:38 -0400

    changes to css

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,21 +1,12 @@
 function init() {
 
-	// var id = document.getElementById('first');
-	// $('html body').animate({
-		// scrollTop : $(id).offset().top
-	// }, 6000, 'easeOutElastic');
-// 	
+	var id = document.getElementById('first');
+	$('html body').animate({
+		scrollTop : $(id).offset().top
+	}, 6000, 'easeOutElastic');
+	
 
 
-if (typeof jQuery != 'undefined') {
- 
-    alert("jQuery library is loaded!");
- 
-}else{
- 
-    alert("jQuery library is not found!");
- 
-}
 }
 
 init();
