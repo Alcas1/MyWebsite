975437a97cd86f30b4ebff11c1a16a1cb2c4e4a3
Alcas1 <jonathanwu70@gmail.com>
2015-05-27 13:24:03 -0400

    changes to css

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,10 +1,10 @@
 function init() {
 
-	var id = document.getElementById('first');
-	$('html body').animate({
-		scrollTop : $("#" + id).offset().top
-	}, 6000, 'easeOutElastic');
-
+	// var id = document.getElementById('first');
+	// $('html body').animate({
+		// scrollTop : $(id).offset().top
+	// }, 6000, 'easeOutElastic');
+	alert("omg");
 }
 
 init();
