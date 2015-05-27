a0e262eef1762ac264dff6592f8d6424cc5f30f1
Alcas1 <jonathanwu70@gmail.com>
2015-05-27 13:51:24 -0400

    changes to css

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,10 +1,20 @@
 function init() {
 
-	$('html').animate({
-		scrollTop : $('first').offset().top
-	}, 6000, 'easeOutElastic');
+
 	
 
+	var $savedList = $("#content_list");
+	$.each(all_objects, function() {
+		$savedList.append("<li><a href='#saved-route'>" + this.text + "</a></li>");
+	});
+	$savedList.delegate("li", "click", function(e) {
+		alert($(this).text());
+	});
+
+	// .addEventListener('click', function(event) {
+	// $('html').animate({
+	// scrollTop : $('first').offset().top
+	// }, 6000, 'easeOutElastic');
 
 }
 
