86d837d040b8a1ffc4d59cb8d4614652ffd25f4e
Alcas1 <jonathanwu70@gmail.com>
2015-05-27 14:49:46 -0400

    changes to css

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,23 +1,22 @@
 function init() {
-	
-	
-	
-	
+
 	var lis = document.getElementById("content_list").getElementsByTagName('li');
 
 	for (var i = 0; i < lis.length; i++) {
 
 		((lis[i].getElementsByTagName('a'))[0]).addEventListener("click", function(e) {
 
-			alert(this.innerHTML);
+			//alert(this.innerHTML);
+			
+			$('html, body').animate({
+				scrollTop : $('#content'+i).offset().top
+			}, 6000, 'easeOutElastic');
+
 		});
 
 	}
 
 	// .addEventListener('click', function(event) {
-	// $('html').animate({
-	// scrollTop : $('first').offset().top
-	// }, 6000, 'easeOutElastic');
 
 }
 
