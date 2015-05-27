8f5464ff58d0778a1c7c6d496a043ed59f25cefd
Alcas1 <jonathanwu70@gmail.com>
2015-05-27 15:22:27 -0400

    changes to css

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -3,9 +3,9 @@ function init() {
 	var lis = document.getElementById("content_list").getElementsByTagName('li');
 
 	for (var i = 0; i < lis.length; i++) {
-		alert(i);
+		
 		lis[i].getElementsByTagName('a')[0].addEventListener("click", function(e) {
-			
+			alert(i);
 			$('html, body').animate({
 				scrollTop : $('#content'+i).offset().top
 			}, 500, 'easeInOutQuint');
