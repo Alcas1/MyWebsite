045b58ddbc4dcd47eebdf0009f89fe41dcb46736
Alcas1 <jonathanwu70@gmail.com>
2015-05-27 14:30:56 -0400

    changes to css

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -3,11 +3,11 @@ function init() {
 	
 	
 	
-	var lis = document.getElementById("content_list").getElementsByTagName('li').getElementsByTagName('a');
+	var lis = document.getElementById("content_list").getElementsByTagName('li');
 
 	for (var i = 0; i < lis.length; i++) {
 
-		lis[i].addEventListener("click", function(e) {
+		lis[i].getElementsByTagName('a').addEventListener("click", function(e) {
 
 			alert(this.innerHTML);
 		});
