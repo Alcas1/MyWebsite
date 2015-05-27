af9c1dc88ecb6363ba5d458587db727026a7e194
Alcas1 <jonathanwu70@gmail.com>
2015-05-27 14:05:49 -0400

    changes to css

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -3,11 +3,11 @@ function init() {
 
 	
 
-	document.getElementById("work_content").addEventListener("click",function(e) {
+	document.getElementById("content_list").addEventListener("click",function(e) {
         // e.target is our targetted element.
                     // try doing console.log(e.target.nodeName), it will result LI
         if(e.target && e.target.nodeName == "LI") {
-            console.log(e.target.id + " was clicked");
+            alert(e.target.id + " was clicked");
         }
     });
 
