769a7cb176e3a85d134e2d72872f7699d87cfa97
Alcas1 <jonathanwu70@gmail.com>
2015-05-28 12:20:05 -0400

    changes to css

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,19 +1,39 @@
 function init() {
-	
+
 	var lis = document.getElementById("content_list").getElementsByTagName('li');
 
 	for (var i = 0; i < lis.length; i++) {
-		
-		lis[i].getElementsByTagName('a')[0].addEventListener("click", function(e) {
+		var listItem = lis[i].getElementsByTagName('a')[0];
+
+		$(window).scroll(function() {
+			if (isScrolledIntoView(listItem)) {
+				$("#top-text").text('s');
+			} else {
+				// do something when element is not viewable
+			}
+		});
+
+		listItem.addEventListener("click", function(e) {
 			$('html, body').animate({
-				scrollTop : $('#content'+e.target.id).offset().top
+				scrollTop : $('#content' + e.target.id).offset().top
 			}, 600, 'easeInOutQuad');
 
 		});
 
 	}
+	$(document).ready(function() {
+		function isScrolledIntoView(elem) {
+			var docViewTop = $(window).scrollTop();
+			var docViewBottom = docViewTop + $(window).height();
 
-	// .addEventListener('click', function(event) {
+			var elemTop = $(elem).offset().top;
+			var elemBottom = elemTop + $(elem).height();
+
+			return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom));
+		}
+
+
+	});
 
 }
 
