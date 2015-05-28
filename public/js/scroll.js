3b54a73898ffe632380239e9e12170e20581fea8
Alcas1 <jonathanwu70@gmail.com>
2015-05-28 15:07:12 -0400

    changes to css

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,14 +1,14 @@
 function init() {
+	var stringItem = "#content" + String(i);
+	var contentItem = $(stringItem).find('div.content_title');
+	var docViewTop = $(window).scrollTop();
+	var distance = $(stringItem).offset().top;
+
+	if (docViewTop >= (distance - 50)) {
+		$("#top-text").text(contentItem.text());
+	}
+
 	$(document).ready(function() {
-
-		var stringItem = "#content" + String(i);
-		var contentItem = $(stringItem).find('div.content_title');
-		var docViewTop = $(window).scrollTop();
-		var distance = $(stringItem).offset().top;
-
-		if (docViewTop >= (distance - 50)) {
-			$("#top-text").text(contentItem.text());
-		}
 
 		var lis = document.getElementById("content_list").getElementsByTagName('li');
 
