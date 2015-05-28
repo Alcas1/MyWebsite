32f01e022571c0480038d2f1ac96db711125bca3
Alcas1 <jonathanwu70@gmail.com>
2015-05-28 15:11:08 -0400

    changes to css

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,18 +1,17 @@
 function init() {
-	
-	
-	$(document).ready(function() {
-		
-		for (var i = 10; i > (10 - lis.length); i--) {
-				var stringItem = "#content" + String(i);
-				var contentItem = $(stringItem).find('div.content_title');
-				var docViewTop = $(window).scrollTop();
-				var distance = $(stringItem).offset().top;
 
-				if (docViewTop >= (distance - 50)) {
-					$("#top-text").text(contentItem.text());
-				}
-			}
+	for (var i = 10; i > (10 - lis.length); i--) {
+		var stringItem = "#content" + String(i);
+		var contentItem = $(stringItem).find('div.content_title');
+		var docViewTop = $(window).scrollTop();
+		var distance = $(stringItem).offset().top;
+		console.log("omg");
+		if (docViewTop >= (distance - 50)) {
+			$("#top-text").text(contentItem.text());
+		}
+	}
+
+	$(document).ready(function() {
 
 		var lis = document.getElementById("content_list").getElementsByTagName('li');
 
