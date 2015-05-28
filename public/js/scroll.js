f832936cf1e52bb70669bc9829daea0507bf74fa
Alcas1 <jonathanwu70@gmail.com>
2015-05-28 15:08:14 -0400

    changes to css

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,12 +1,5 @@
 function init() {
-	var stringItem = "#content" + String(i);
-	var contentItem = $(stringItem).find('div.content_title');
-	var docViewTop = $(window).scrollTop();
-	var distance = $(stringItem).offset().top;
-
-	if (docViewTop >= (distance - 50)) {
-		$("#top-text").text(contentItem.text());
-	}
+	
 
 	$(document).ready(function() {
 
