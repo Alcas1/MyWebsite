daab91b84d41b9030f70ebdc47b7a9c427f6ee91
Alcas1 <jonathanwu70@gmail.com>
2015-05-28 19:45:51 -0400

    changes to css

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,7 +1,7 @@
 function init() {
-	
+
 	$(document).ready(function() {
-		var top_element=10;
+		var top_element = 10;
 		var lis = document.getElementById("content_list").getElementsByTagName('li');
 		var semiTop = 0;
 		for (var i = top_element; i > (top_element - lis.length); i--) {
@@ -31,37 +31,30 @@ function init() {
 
 		$(window).scroll(function() {
 			var docViewTop = $(window).scrollTop();
-			
+
 			for (var i = top_element; i > (top_element - lis.length); i--) {
 				var stringItem = "#content" + String(i);
 				var contentItem = $(stringItem).find('div.content_title');
-				
+
 				var distance = $(stringItem).offset().top;
 				if (i === top_element) {
 					semiTop = distance;
 					if (docViewTop < (semiTop - 150)) {
 						$("#top-text").text("Works");
 					}
-					
-					if(docViewTop>=(distance-50))
-					{
-						$("#top_back").css("position","fixed");
-					}
-					else
-					{
-						$("#top_back").css("position","absolute");
-					}
-					
+
 				}
 				if (docViewTop >= (distance - 50)) {
 					$("#top-text").text(contentItem.text());
 				}
 			}
-			
-			
-			
-			
-			
+
+			if (docViewTop >= (semiTop - 50)) {
+				$("#top_back").css("position", "fixed");
+			} else {
+				$("#top_back").css("position", "absolute");
+			}
+
 		});
 	});
 
