67eb2e878717e83c0d80be43de15dc6782d1d460
Alcas1 <jonathanwu70@gmail.com>
2015-05-29 01:17:06 -0400

    changes to css

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -83,7 +83,7 @@ function init() {
 				//console.log(top_height);
 				$("#content10 #back_top").css("position", "absolute");
 				$("#content10 #back_top").css("top", ($(window).height()-150)+"px");
-				$("#content10 #back_top").css("left", ($(window).width()-($("content10").offset().left) -125) + "px");
+				$("#content10 #back_top").css("left", ($(window).width()-($("#content10").offset().left) -125) + "px");
 			}
 
 		});
