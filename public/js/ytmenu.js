f0a8dd7a42f32a5458ac0dec2db28a1ac0b2e252
Alcas1 <jonathanwu70@gmail.com>
2015-05-29 14:00:12 -0400

    changes to css

--- public/js/ytmenu.js
+++ public/js/ytmenu.js
@@ -1,8 +1,8 @@
 jQuery.fn.center = function() {
-	this.css("position", "fixed");
-	this.css("top", Math.max(0, (($(window).outerHeight() - $(this).outerHeight()/2-$(this).height())) + "px"));
-	this.css("left", Math.max(0, (($(window).outerWidth() - $(this).outerWidth()/2-$(this).width())) + "px"));
-	return this;
+	 this.css("position","fixed");
+    this.css("top", ($(window).height() / 2) - (this.outerHeight() / 2));
+    this.css("left", ($(window).width() / 2) - (this.outerWidth() / 2));
+    return this;
 };
 
 window.onload = (function() {
