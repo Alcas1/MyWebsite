7f635abc0c4a8c0a734cbab81dac257708402e21
Alcas1 <jonathanwu70@gmail.com>
2015-05-29 14:02:07 -0400

    changes to css

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -113,10 +113,11 @@ function init() {
 					
 				},50);
 				$('#content_img_small').css("position","fixed");
-				$('#content_img_small').center();
+				
 				$('#content_img_small').animate({
 					maxWidth: (($(window).width()*.8 < $(window).height()*.8)? $(window).width()*.8:$(window).height()*.8)
 				});
+				$('#content_img_small').center();
 			}
 
 		}, false);
