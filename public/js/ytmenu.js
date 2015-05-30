4e5560579c862b4df24050c888c2fcfcf47f59bf
Alcas1 <jonathanwu70@gmail.com>
2015-05-30 18:09:48 -0400

    changes to css

--- public/js/ytmenu.js
+++ public/js/ytmenu.js
@@ -38,7 +38,7 @@ window.onload = (function() {
 					}
 					if(site===6)
 					{
-						$(this).children('a').css("width","194px");
+						$(this).children('a').css("width","193px");
 					}
 					
 				}
