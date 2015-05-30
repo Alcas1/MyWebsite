7df96ae2e9be3a0d54066791582841e3b9263595
Alcas1 <jonathanwu70@gmail.com>
2015-05-30 03:46:08 -0400

    changes to css

--- public/js/ytmenu.js
+++ public/js/ytmenu.js
@@ -15,7 +15,7 @@ window.onload = (function() {
 			var trigger = el.querySelector('div.dr-trigger'), icon = trigger.querySelector('span.dr-icon-menu'), open = false;
 			var site = checkSite();
 			
-			$('.dr-menu ul').children().each(function(idx, val) {
+			$('.dr-menu ul').children().toArray().each(function(idx, val) {
 				if(idx<8)
 				{
 					if(idx===site)
