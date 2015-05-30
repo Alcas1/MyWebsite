ad050c35eec339e706fb1d5d175721a6f24820d8
Alcas1 <jonathanwu70@gmail.com>
2015-05-30 15:57:42 -0400

    changes to css

--- public/js/ytmenu.js
+++ public/js/ytmenu.js
@@ -21,17 +21,17 @@ window.onload = (function() {
 						var el=$(this).first().first();
 						el.css("border-right-color", "#03a9f4");
 						el.css("border-right-style", "solid");
-						el.css("border-right-width", "4px");
-						el.css("margin-left","-2px");
+						el.css("border-right-width", "2px");
+						el.css("margin-left","-1px");
 						el.css("font-weight", "700");
 						$(this).first().first().mouseenter(function() {
 							el.css("border-right-color", "#03a9f4");
 							el.css("border-right-style", "solid");
-							el.css("border-right-width", "2px");
+							el.css("border-right-width", "1px");
 						}).mouseleave(function() {
 							el.css("border-right-color", "#03a9f4");
 							el.css("border-right-style", "solid");
-							el.css("border-right-width", "4px");
+							el.css("border-right-width", "2px");
 						});
 					}
 				}
