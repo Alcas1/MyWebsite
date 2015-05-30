45f7f1f07e86e90caae5178c645e10d3c3ba3e2a
Alcas1 <jonathanwu70@gmail.com>
2015-05-30 02:31:00 -0400

    changes to css

--- public/js/ytmenu.js
+++ public/js/ytmenu.js
@@ -16,7 +16,13 @@ window.onload = (function() {
 
 			trigger.addEventListener('click', function(event) {
 				if (!open) {
-					checkSite();
+					var site=checkSite();
+					if(i===site)
+					{
+						trigger.css("font","bold");
+					}
+					
+					
 					el.className += ' dr-menu-open';
 					open = true;
 					var B = document.body, H = document.documentElement, height;
