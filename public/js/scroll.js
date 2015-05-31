5c02a0fd4f73aed7e7b725c125499d60e20e1c1e
Alcas1 <jonathanwu70@gmail.com>
2015-05-31 13:41:38 -0400

    changes to css

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -192,7 +192,7 @@ function fillImage(imageNumber) {
 				top : '50%',
 				left : '50%'
 			}, 300);
-			console.log((((jImg.height() > jImg.width()) ? jImg.width() : jImg.height()) * 2) - 100);
+			console.log(jImg.width());
 			jCaption.animate({
 				marginTop : ((-max / 2) + ((jImg.height() > jImg.width()) ? jImg.width() : jImg.height()) * 2) - 100 + "px",
 				marginLeft : (-max / 2),
