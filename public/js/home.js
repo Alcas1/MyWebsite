2e8216372b5f8c362de71ae1acfa3e64c3de5b19
Alcas1 <jonathanwu70@gmail.com>
2015-06-01 17:54:49 -0400

    changes to css

--- public/js/home.js
+++ public/js/home.js
@@ -1,14 +1,18 @@
-
-
-
-
-
 function init() {
-	
 
+	var el = $('#part_body_card');
 
+	el.mouseenter(function(e) {
+		alert("omg");
+		el.animate({
+			boxShadow : "box-shadow: 0px 2px 12px rgba(0, 0, 0, 0.4)"
 
-
-
+		}, 100);
+	});
+	el.mouseleave(function(e) {
+		el.animate({
+			boxShadow : "box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.4)"
+		}, 100);
+	});
 
 };
