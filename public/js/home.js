dd50a9562b924a921d4c9824f9fd6f050b9f5c53
Alcas1 <jonathanwu70@gmail.com>
2015-06-01 18:21:50 -0400

    changes to css

--- public/js/home.js
+++ public/js/home.js
@@ -1,18 +1,20 @@
-function init() {
-	$(window).load(function() {
-		var el = $('#part_body_card');
-		alert("omg");
-		el.mouseenter(function(e) {
-			alert("omg");
-			el.animate({
-				boxShadow : "box-shadow: 0px 2px 12px rgba(0, 0, 0, 0.4)"
+$(window).load(function() {
+	
+	$('#part4').children().each(function(idx, val) 
+	{
+		$(this).mouseenter(function(e) {
+			
+			$(this).animate({
+				boxShadow : "0px 2px 12px rgba(0, 0, 0, 0.4)"
 
 			}, 100);
 		});
-		el.mouseleave(function(e) {
-			el.animate({
-				boxShadow : "box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.4)"
+		$(this).mouseleave(function(e) {
+			$(this).animate({
+				boxShadow : "0px 2px 6px rgba(0, 0, 0, 0.4)"
 			}, 100);
 		});
+
 	});
-};
+});
+
