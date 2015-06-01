467246fb6cab32bc30696f3849fa7e8adbf81b4d
Alcas1 <jonathanwu70@gmail.com>
2015-06-01 18:00:54 -0400

    changes to css

--- public/js/home.js
+++ public/js/home.js
@@ -1,18 +1,18 @@
 function init() {
-
-	var el = $('#part_body_card');
-
-	el.mouseenter(function(e) {
+	$(window).load(function() {
+		var el = $('#part_body_card');
 		alert("omg");
-		el.animate({
-			boxShadow : "box-shadow: 0px 2px 12px rgba(0, 0, 0, 0.4)"
+		el.mouseenter(function(e) {
+			alert("omg");
+			el.animate({
+				boxShadow : "box-shadow: 0px 2px 12px rgba(0, 0, 0, 0.4)"
 
-		}, 100);
+			}, 100);
+		});
+		el.mouseleave(function(e) {
+			el.animate({
+				boxShadow : "box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.4)"
+			}, 100);
+		});
 	});
-	el.mouseleave(function(e) {
-		el.animate({
-			boxShadow : "box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.4)"
-		}, 100);
-	});
-
 };
