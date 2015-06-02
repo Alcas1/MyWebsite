70d4e56f2e8c0d01d40f3d1b321ed75e56d5d13c
Alcas1 <jonathanwu70@gmail.com>
2015-06-02 16:42:02 -0400

    changes to css

--- public/js/home.js
+++ public/js/home.js
@@ -2,13 +2,14 @@ $(window).load(function() {
 
 	$('#text_card').children().each(function(idx, val) {
 		$(this).mouseenter(function(e) {
-
+			$(this).stop();
 			$(this).animate({
 				boxShadow : "0px 0px 20px rgba(0, 0, 0, 0.4)"
-
+				
 			}, 200);
 		});
 		$(this).mouseleave(function(e) {
+			$(this).stop();
 			$(this).animate({
 				boxShadow : "0px 2px 6px rgba(0, 0, 0, 0.4)"
 			}, 200);
