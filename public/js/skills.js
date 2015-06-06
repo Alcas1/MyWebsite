657c9d9f94b689bc2e99415d6537768f4f15e520
Alcas1 <jonathanwu70@gmail.com>
2015-06-06 02:23:18 -0400

    changes to css

--- public/js/skills.js
+++ public/js/skills.js
@@ -10,6 +10,45 @@ $(document).ready(function() {
 	var mobile_stars = [4, 5];
 
 	set_stars(web_stars, 1);
+	var top_element = 10;
+	var lis = document.getElementById("skills_list").getElementsByTagName('li');
+	var semiTop = 0;
+	for (var i = 0; i < lis.length; i++) {
+		var listItem = lis[i].getElementsByTagName('a')[0];
+
+		listItem.addEventListener("click", function(e) {
+
+			$('html, body').animate({
+				scrollTop : $('#skill_' + e.target.id).offset().top-50
+			}, 600, 'easeInOutQuad');
+
+		});
+
+	}
+
+	$(window).scroll(function() {
+		var docViewTop = $(window).scrollTop();
+
+		for (var i = top_element; i > (top_element - lis.length); i--) {
+			var stringItem = "#skill_" + String(i);
+			var contentItem = $(stringItem).find('div.skills_title');
+
+			var distance = $(stringItem).offset().top;
+			if (i === top_element) {
+				semiTop = distance;
+				if (docViewTop < (semiTop - 150)) {
+					$("#top-text").text("Skills");
+				}
+
+			}
+			if (docViewTop >= (distance - 50)) {
+
+				$("#top-text").text(contentItem.text());
+			}
+
+		}
+
+	});
 
 });
 
@@ -24,10 +63,10 @@ function set_stars(stars, type) {
 
 			$(this).find('img').eq(j).attr('src', 'imgs/skills/full_star.png');
 			// $(this).find('img').eq(j).delay(1000).animate({
-				// maxWidth : "30px"
+			// maxWidth : "30px"
 			// }, 300);
 			// $(this).find('img').eq(j).animate({
-				// maxWidth : "28px"
+			// maxWidth : "28px"
 			// }, 300);
 
 		}
