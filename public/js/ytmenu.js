cf1a74f0da692b93df6772d218ebdf5daa7ae78e
Alcas1 <jonathanwu70@gmail.com>
2015-06-08 01:43:12 -0400

    changes to css

--- public/js/ytmenu.js
+++ public/js/ytmenu.js
@@ -5,21 +5,7 @@ jQuery.fn.center = function() {
 
 	return this;
 };
-function processAjaxData(response, urlPath) {
-	document.getElementById("content").innerHTML = response.html;
-	document.title = response.pageTitle;
-	window.history.pushState({
-		"html" : response.html,
-		"pageTitle" : response.pageTitle
-	}, "", urlPath);
-}
 
-window.onpopstate = function(e) {
-	if (e.state) {
-		document.getElementById("content").innerHTML = e.state.html;
-		document.title = e.state.pageTitle;
-	}
-};
 
 window.onload = (function() {
 
