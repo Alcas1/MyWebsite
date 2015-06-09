3de3ce73213ae690422c28b8e2691817b210e212
Alcas1 <jonathanwu70@gmail.com>
2015-06-09 00:49:35 -0400

    changes to css

--- public/js/skills.js
+++ public/js/skills.js
@@ -8,14 +8,14 @@ $(document).on('ready', function() {
 	var web_stars = [4.5, 4, 3.5, 4, 4, 4];
 	var backend_stars = [3.5, 3.5, 5, 3, 5];
 	var mobile_stars = [4, 5];
-	var tools_stars = [5,4.5,4];
-	var env_stars = [5,4.5];
-	
+	var tools_stars = [5, 4.5, 4];
+	var env_stars = [5, 4.5];
+
 	set_stars(web_stars, 1);
 	set_stars(backend_stars, 2);
 	set_stars(mobile_stars, 3);
-	set_stars(tools_stars,4);
-	set_stars(env_stars,5);
+	set_stars(tools_stars, 4);
+	set_stars(env_stars, 5);
 
 	var top_element = 10;
 	var lis = document.getElementById("skills_list").getElementsByTagName('li');
@@ -62,12 +62,11 @@ $(document).on('ready', function() {
 function set_stars(stars, type) {
 	// var mHeight = ((stars.length / 3.0)) * 160;
 	// if (stars.length < 3) {
-		// mHeight = 160;
+	// mHeight = 160;
 	// }
 	// $('#skill_' + (11 - type) + ' .skill_card').css("min-height", mHeight + "px");
 
-	var star_class;
-	star_class = $('#skill_' + (11 - type) + ' .skill_card .part_body_card_skill #web_stars');
+	var star_class = $('#skill_' + (11 - type) + ' .skill_card .part_body_card_skill #web_stars');
 	star_class.each(function(idx) {
 		var j;
 		for ( j = 0; j < stars[idx] - 0.5; j++) {
@@ -92,13 +91,13 @@ function set_stars(stars, type) {
 				next();
 			}).delay(100 * j);
 		}
-		
-	});
-	if(stars[idx]===5)
-		{
-			console.log(stars[idx]);
-			$('#skill_' + (11 - type) + ' .skill_card .part_body_card_skill').css('boxShadow', "0px 1px 15px #03a9f4");
-		}
 
+	});
+	star_class = $('#skill_' + (11 - type) + ' .skill_card .part_body_card_skill');
+	star_class.each(function(idx) {
+		if (stars[idx] === 5) {
+			$(this).css('boxShadow', "0px 1px 15px #03a9f4");
+		}
+	});
 }
 
